ae7f30dcd37757794f37cae3742b81c4994dc801
sungbum05 <sblim05@naver.com>
2022-07-09 17:29:02 +0900

    CardMaking_30%

--- 2022GameJamJam_Team5/Assets/Script/Card/CardData.cs
+++ 2022GameJamJam_Team5/Assets/Script/Card/CardData.cs
@@ -8,13 +8,30 @@ public enum CardType
 }
 
 [System.Serializable]
-public class CardData : MonoBehaviour
+public class CardData
 {
-    public string CardName;
+    [Header("외부 속성")]
+    public InGameMgr InGameMgr;
+
+    [Header("카드 내부 속성")]
+    public bool IsCardUnlock = false;
     public CardType CardType;
-    
+
+    [Header("카드 외부 속성")]
+    public string CardName;
+    public Sprite CardImg;
+    public string CardExplanation;
+
+    [Header("카드 이펙트 이미지")]
+    public Sprite CardEffectImg;
+
+    CardData()
+    {
+        Debug.Log("asd");
+    }    
+
     public virtual void CardEffect()
     {
-
+        
     }
 }
